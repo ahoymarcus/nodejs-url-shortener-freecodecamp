fe39f7f8dc505e5565f7e386de4f2cf84ce198ed
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-10-06 22:31:56 -0300

    Inserir rota api-shorturl

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+
 const app = express();
 
 // Basic Configuration
@@ -10,14 +14,38 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+
+
+app.post("/api/shorturl", function(req, res) {
+	console.log(req.body);
+	
+	res.json({
+		"original_url" "original_url",
+		"short_url": "short_url"
+	});
+	
+	// res.json({ error: 'invalid url' });
+});
+
+
+
+app.get("/api/shorturl/:shorturl", function(req, res) {
+	
 });
 
 
@@ -27,6 +55,8 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
 
 
 
